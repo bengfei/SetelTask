187b1f63e7c0e40f7afcaf6633bf46186eca4b85
bengfei <bengfei2008@gmail.com>
2019-11-26 00:42:44 +0800

    Add files via upload

---
+++ node-orderSystem/node-orderSystem/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    localport: 4000,    
+    
+    mongoserver: '127.0.0.1',
+    mongodbname: 'SetelDB',
+    //payment system info
+    paymentport: 4001,
+    paymentServerIp: '127.0.0.1',
+
+    freq: 15,
+   
+};
